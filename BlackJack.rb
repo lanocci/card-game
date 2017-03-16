4e4f94f75f935c3996eced69654f9985f96acc03
Koichi Urano <scsk-urano@S-028464.ciidmn.local>
2017-03-16 19:00:42 +0900

    add .gitignore

--- BlackJack.rb
+++ BlackJack.rb
@@ -14,5 +14,22 @@ class BlackJackHand < Hand
     end
     return maxUnder == MIN_VALUE ? minOver : maxUnder
   end
-  
+
+  def possibleScores
+  end
+
+  def busted
+    return score > 21
+  end
+
+  def is21
+    return score == 21
+  end
+
+  def isBlackJack
+    return false if @cards.size != 2
+    BlackJackCard first = @cards.get(0)
+    BlackJackCard second = @cards.get(1)
+    return first.ace? && second.faceCard? || second.ace? && faceCard?
+  end
 end
